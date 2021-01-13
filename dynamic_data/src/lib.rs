c5cbb41c16e7cee824133d3c24387f7034fe72fa
GTimothy <22472919+GTimothy@users.noreply.github.com>
2021-01-13 18:21:03 +0100

    POC for dynamic data usage in widgets

---
+++ dynamic_data/src/lib.rs
@@ -0,0 +1,70 @@
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Previous(isize),
+    Next(isize),
+}
+pub trait DataLoader<T> {
+    fn get_next_n(&self, n: isize, from: isize) -> Vec<T>;
+    fn get_previous_n(&self, n: isize, from: isize) -> Vec<T>;
+}
+pub struct DynamicData<'a, T, D: DataLoader<T>> {
+    pub state: &'a mut State<T, D>,
+}
+
+pub struct State<T, D>
+where
+    D: DataLoader<T>,
+{
+    current_start: isize,
+    capacity: usize,
+    data_source: D,
+    data: Vec<T>,
+}
+
+impl<T, D: DataLoader<T>> State<T, D> {
+    pub fn new(data_source: D) -> Self {
+        State {
+            current_start: 0,
+            capacity: 40,
+            data_source,
+            data: Vec::new(),
+        }
+    }
+    pub fn current_start(mut self, s: isize) -> Self {
+        self.current_start = s;
+        self
+    }
+
+    pub fn capacity(mut self, k: usize) -> Self {
+        self.capacity = k;
+        self
+    }
+
+    pub fn data(&self) -> &Vec<T> {
+        &self.data
+    }
+
+    pub fn fetch_next(&mut self, n: isize) {
+        let from = self.current_start + (self.data.len() as isize);
+        let data_to_add = self.data_source.get_next_n(n, from);
+        let over_capacity = (data_to_add.len() + self.data.len()) as isize - self.capacity as isize;
+
+        if over_capacity > 0 {
+            self.data = self.data.split_off(over_capacity as usize);
+            self.current_start += over_capacity as isize;
+        }
+
+        self.data.extend(data_to_add);
+    }
+
+    pub fn fetch_previous(&mut self, n: isize) {
+        let from = self.current_start;
+        let mut data_to_add = self.data_source.get_previous_n(n, from);
+        self.current_start -= data_to_add.len() as isize;
+        self.data.reverse();
+        data_to_add.reverse();
+        self.data.extend(data_to_add);
+        self.data.reverse();
+        self.data.truncate(self.capacity);
+    }
+}
---
+++ dynamic_data/src/main.rs
@@ -0,0 +1,86 @@
+use dynamic_data::Message;
+use iced_graphics::Renderer;
+use iced_native::Element;
+use iced_wgpu::Backend;
+
+use iced::{button, Application, Button, Column, Settings};
+
+struct DummyEntryLoader();
+
+struct Entry(isize);
+
+impl dynamic_data::DataLoader<Entry> for DummyEntryLoader {
+    fn get_next_n(&self, n: isize, from: isize) -> Vec<Entry> {
+        (from..from + n).map(|i| Entry(i)).collect()
+    }
+
+    fn get_previous_n(&self, n: isize, from: isize) -> Vec<Entry> {
+        (from - n..from).map(|i| Entry(i)).collect()
+    }
+}
+
+pub fn main() -> iced::Result {
+    Model::run(Settings::default())
+}
+
+struct Model {
+    data_state: dynamic_data::State<Entry, DummyEntryLoader>,
+    next_button: button::State,
+    previous_button: button::State,
+    scrollable_state: iced::scrollable::State,
+}
+
+impl iced::Sandbox for Model {
+    type Message = Message;
+
+    fn title(&self) -> String {
+        String::from("DynamicData - Iced")
+    }
+
+    fn new() -> Self {
+        Model {
+            data_state: dynamic_data::State::new(DummyEntryLoader {}).capacity(100),
+            scrollable_state: iced::scrollable::State::new(),
+            previous_button: button::State::new(),
+            next_button: button::State::new(),
+        }
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Previous(n) => self.data_state.fetch_previous(n),
+            Message::Next(n) => self.data_state.fetch_next(n),
+        }
+    }
+
+    fn view(&mut self) -> Element<Message, Renderer<Backend>> {
+        let dd = dynamic_data::DynamicData {
+            state: &mut self.data_state
+        };
+
+        // This is where you choose how you want to display your data. Here it is a column with
+        // text widgets, but it could be a canvas, a row, one of your own widgets, anything you
+        // want.
+        let data_column: Column<Message> =
+            dd.state.data().into_iter().fold(Column::new(), |col, i| {
+                col.push(iced::Text::new(i.0.to_string()))
+            });
+
+        let previous_button =
+            Button::new(&mut self.previous_button, iced::Text::new("Load previous"))
+                .on_press(Message::Previous(20));
+
+        let next_button = Button::new(&mut self.next_button, iced::Text::new("Load next"))
+            .on_press(Message::Next(40));
+
+        iced::Scrollable::new(&mut self.scrollable_state)
+            .push(
+                Column::new()
+                    .push(previous_button)
+                    .push(data_column)
+                    .push(next_button),
+            )
+            .height(iced::Length::Fill)
+            .into()
+    }
+}
